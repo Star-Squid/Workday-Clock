5870f14fd345c35c7ce20d2f7c58ad9c8159a0ae
Barbara <barbara.bijakowska@gmail.com>
2022-02-15 18:07:14 +0000

    try a method to change main dome colour, fail

--- index.js
+++ index.js
@@ -10,14 +10,44 @@ function updateTime(){
     if (minutes < 10){
         $(".currenthour").text(smallTimeAsText);
     } else {$(".currenthour").text(timeAsText);
-};
+    };
 
 //out of hours title
     if (hours >= 18){$("h1").text("stop working")};
 
 //change gradient on big dome
 
+    if (minutes < 5){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 10){
+        $(".main-dome").addClass(".for5to10");     
+    } else if (minutes < 15){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 20){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 25){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 30){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 35){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 40){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 45){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 50){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 55){
+        $(".main-dome").addClass(".for5to10");
+    } else if (minutes < 60){
+        $(".main-dome").addClass(".for5to10");
+    } else {
+        console.log(minutes)
+    };
+
+
 //change gradient on small dome
+
 }
 
 setInterval(updateTime, 1000);
