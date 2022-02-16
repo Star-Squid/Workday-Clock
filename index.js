8319ab652b79031d8f850b53fcc2c225cf3b1b29
Barbara <barbara.bijakowska@gmail.com>
2022-02-16 13:21:25 +0000

    achieve a working main dome

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 function updateTime(){
     var currentTime = new Date();
     var hours = currentTime.getHours();
@@ -6,48 +7,35 @@ function updateTime(){
     var timeAsText = hours + " : " + minutes;
     var smallTimeAsText = hours + " : 0" + minutes;
     
-//update bottom hour
+    //update bottom hour
     if (minutes < 10){
         $(".currenthour").text(smallTimeAsText);
     } else {$(".currenthour").text(timeAsText);
-    };
+};
 
 //out of hours title
-    if (hours >= 18){$("h1").text("stop working")};
+if (hours >= 18){$("h1").text("stop working")};
 
-//change gradient on big dome
+//convert time to degree of dome
+var degFromHour = ((hours - 9) * 20);
+var degFromMinute = minutes / 3;
+var mainDomeDeg = Math.floor(degFromHour + degFromMinute) + "deg";
+var smallDomeDeg = minutes * 3 + "deg";
 
-    if (minutes < 5){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 10){
-        $(".main-dome").addClass(".for5to10");     
-    } else if (minutes < 15){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 20){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 25){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 30){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 35){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 40){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 45){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 50){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 55){
-        $(".main-dome").addClass(".for5to10");
-    } else if (minutes < 60){
-        $(".main-dome").addClass(".for5to10");
-    } else {
-        console.log(minutes)
-    };
+console.log("Main degrees:" + mainDomeDeg + " Small degrees:" + smallDomeDeg);
 
+//change gradient on main dome
+
+
+document.documentElement.style
+.setProperty('--timeasdegree', mainDomeDeg);
 
 //change gradient on small dome
-
-}
-
-setInterval(updateTime, 1000);
+                                                    
+};
+                                                
+updateTime();
+setInterval(updateTime, 10000);
+var result = getComputedStyle(document.documentElement)
+   .getPropertyValue('--timeasdegree');
+console.log(result);
